a322e28bff444767994ee7d5fce8fa7b8240f382
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-12-23 19:33:34 +0200

    final changes delete inrelevent files

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -6,24 +6,6 @@ import bgimage from "../assets/shenk-5.jpg";
 import mainImageOne from "../assets/reka/or shenkerman x Atlas @or.shenk-4-2.jpg";
 import mainImageTwo from "../assets/reka/bigImageOne-.jpg";
 // small images
-import colaz1 from "../assets/ragil/colaz1.jpg";
-import DSC00036 from "../assets/ragil/DSC00036.jpg";
-import DSC01314 from "../assets/ragil/DSC01314.jpg";
-import DSC02179 from "../assets/ragil/DSC02179.jpg";
-import DSC04303 from "../assets/ragil/DSC04303.jpg";
-import DSC06434 from "../assets/ragil/DSC06434.jpg";
-import DSC06514 from "../assets/ragil/DSC06514.jpg";
-import DSC06867 from "../assets/ragil/DSC06867.jpg";
-import DSC06927 from "../assets/ragil/DSC06927.jpg";
-import DSC09719 from "../assets/ragil/DSC09719.jpg";
-import DSC09930 from "../assets/ragil/DSC09930 (2).jpg";
-import or1 from "../assets/ragil/or shenkerman x Atlas @or.shenk-1-2.jpg";
-import orshenk22 from "../assets/ragil/or shenkerman x Atlas @or.shenk-2-2.jpg";
-import colaz4 from "../assets/ragil/or shenkerman x Atlas @or.shenk-2-3.jpg";
-import colaz5 from "../assets/ragil/or shenkerman x Atlas @or.shenk-3-2.jpg";
-import colaz6 from "../assets/ragil/or shenkerman x Atlas @or.shenk-4-3.jpg";
-import colaz7 from "../assets/ragil/or shenkerman x Atlas @or.shenk-5-2.jpg";
-import colaz8 from "../assets/ragil/or shenkerman x Atlas @or.shenk-11.jpg";
 
 function Projects() {
   return (
